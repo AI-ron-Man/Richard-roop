769196a796a145b6c0d6ba08f8b175e74fbe83b4
Dara Ajayi <ooajayi99@gmail.com>
2023-07-27 10:18:52 -0400

    Change input file names

--- run.gpu.sh
+++ run.gpu.sh
@@ -6,4 +6,4 @@ docker run -it --rm \
     -v ~/ML/models/.insightface/:/root/.insightface/ \
     -v ~/ML/models/.opennsfw2/:/root/.opennsfw2/ \
     richard-roop-gpu \
-    -f ./media/michelle-obama.jpg -t ./media/serena-red.jpg -o ./media/output___0_.jpg
+    -f ./media/source.jpg -t ./media/target.jpg -o ./media/output.jpg
